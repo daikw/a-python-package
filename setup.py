dfdfcbe8296e0668da4dc7537d1acf8e409cd936
daikw <daikw@yak-shaver.com>
2022-10-10 11:32:36 +0900

    :tada:
    Signed-off-by: daikw <daikw@yak-shaver.com>

---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="myapp",
+    version="0.0.1",
+    license='MIT',
+    description="sample app.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/daikw/a-python-package",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    packages=setuptools.find_packages(),
+    python_requires=">=3.7",
+)
